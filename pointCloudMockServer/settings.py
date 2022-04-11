db07c5bc0f4dab1ae90da5b135f9e8dea493bc5e
lihonghao <940512354@qq.com>
2022-04-11 15:46:16 +0800

    allowed all ip

--- pointCloudMockServer/settings.py
+++ pointCloudMockServer/settings.py
@@ -126,7 +126,7 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 
 USE_TZ = True
-
+ALLOWED_HOSTS = ['*']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
